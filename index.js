1840d5c44aea05999377d8081987a1806b21fac9
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2020-06-01 17:06:49 -0400

    commit before browerify tests

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 const express = require('express')
 const app = express();
+const path = require('path');
+const public = path.join(__dirname, 'public') //This is the path where all static files (html, js, css, assets) will be served from
+
+
+app.use(express.static(public));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(public + '/index.html'))
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Spinning up warp drives')
 });
---
+++ public/js/mapbox.js
@@ -0,0 +1,13 @@
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25icmV6ZWwiLCJhIjoiY2pwNXNyb3IxMDJwZTNxbzZ4M3IxdGp5ZCJ9.1_DVjqU_cgiK9gt-LGf3DA';
+
+
+$(document).ready(function(){ //call when the document is loaded
+    console.log("Ready!")
+    var map = new mapboxgl.Map({
+        container: 'mapBox',
+        style: 'mapbox://styles/aaronbrezel/ckawuj5n200b71io1wjxynuji'
+      });
+      
+})
